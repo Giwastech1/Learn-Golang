450b81720abba996d2608888885a68e44aedb469
giwastech1 <giwatoheeb27@gmail.com>
2025-11-11 00:13:22 +0100

    creating array with make function

--- arraySlicing.go
+++ arraySlicing.go
@@ -7,8 +7,17 @@ func sliceArray(){
 	fmt.Println(myArray)
 	myNewArray := myArray[1:3]
 	fmt.Println(myNewArray)
+	fmt.Println(cap(myNewArray))
+	myNewArray = myArray[1:5]
+	fmt.Println(myNewArray)
+}
+
+func sliceWithMake(){
+	myArray := make([] int, 8)
+	fmt.Println(myArray)
 }
 
 func main(){
 	sliceArray()
+	sliceWithMake()
 }
